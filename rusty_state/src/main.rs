98360bd5e97bfb5afebdeaf681364f8e5d4adea3
Artur Serra <artur_seilah@hotmail.com>
2022-11-19 20:23:27 -0300

    Added state, player and terminal greetings

---
+++ rusty_state/src/main.rs
@@ -0,0 +1,77 @@
+#![warn(clippy::pedantic)]
+
+use bracket_lib::prelude::*;
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Rusty Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You destroyed the city!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.game_over(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+fn main() -> BError{
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusty Dragon")
+        .build()?;
+
+    main_loop(context, State::new())
+}
--- terminal/src/main.rs
+++ terminal/src/main.rs
@@ -5,13 +5,13 @@ struct State {}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls(); 
-        ctx.print(1, 1, "Hello, Masha!"); 
+        ctx.print(1, 1, "Hello, Andruil!"); 
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50() 
-        .with_title("Flappy Dragon") 
+        .with_title("Rusty Dragon") 
         .build()?; 
 
     main_loop(context, State{})
